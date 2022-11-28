9d533a010f4093b068f4e6bc2410fb90c287564d
francescotrudu <francescotrudu84@gmail.com>
2022-11-28 16:49:07 +0100

    script one

---
+++ script.js
@@ -0,0 +1,7 @@
+// Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
+
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let question = prompt("Inserire un numero da 1 a 10");
+console.log(question);
+
