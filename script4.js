e2fb0830495922f627fa8d8b780774b67aeb0b92
francescotrudu <francescotrudu84@gmail.com>
2022-11-28 19:48:28 +0100

    combine two arrays

---
+++ script4.js
@@ -0,0 +1,14 @@
+// Prendere due array della stessa lunghezza e generarne un terzo,
+// prendendo alternativamente gli elementi da uno e dall'altro es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+const arrayOne = ["Marco", "Lavinia", "Carlos", "Giada", "Sebastiano", "Domenico"];
+const arrayTwo = ["Rossi", "Frizzi", "Perez", "Gallitto", "Urban", "Montrone"];
+
+    const combineArray = [];
+
+    for (let i = 0; i < arrayOne.length; i += 1) {
+        combineArray.push(arrayOne[i]);
+        combineArray.push(arrayTwo[i]);
+    }
+
+console.log(combineArray)
