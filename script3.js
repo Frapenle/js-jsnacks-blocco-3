ee66413fbb1725724cc53282d3b81b7484722c35
francescotrudu <francescotrudu84@gmail.com>
2022-11-28 18:14:07 +0100

    script3

--- script3.js
+++ script3.js
@@ -1,13 +1,24 @@
 // Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 
 const arrayOne = [1, 2, 3, 4, 5, 6, 7];
-const arrayTwo = [11, 33];
-let arrayDiff;
+const arrayTwo = [11, 33, 9, 44, 5, 55, 23, 1, 2, 3, 4, 5, 6, 7];
 
+//---- mi son perso----
+// let arrayDiff;
+// if (arrayOne.length >= arrayTwo.length) {
+//     arrayDiff = (arrayOne.length - arrayTwo.length);
+//     console.log(arrayDiff);
+// } else {
+//     arrayDiff = arrayTwo.length - arrayOne.length;
+//     console.log(arrayDiff);
+// }
 
-if (arrayOne.length >= arrayTwo.length) {
-    arrayDiff = parseInt(arrayOne.length - arrayTwo.length);
-    console.log(arrayDiff)
-} else {
-    arrayDiff = parseInt(arrayTwo.length - arrayOne.length);
+while (arrayOne.length > arrayTwo.length) {
+    arrayTwo.push(4);
 }
+
+while (arrayTwo.length > arrayOne.length) {
+    arrayOne.push(1)
+}
+console.log(arrayOne);
+console.log(arrayTwo);
